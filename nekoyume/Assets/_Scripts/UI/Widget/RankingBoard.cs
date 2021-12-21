153b611ecafd3c7f2f5ba645850a64989b560935
jiwon <atralupus@icloud.com>
2021-12-21 17:01:00 +0900

    Add booster repeat slider ui

--- nekoyume/Assets/_Scripts/UI/Widget/RankingBoard.cs
+++ nekoyume/Assets/_Scripts/UI/Widget/RankingBoard.cs
@@ -473,7 +473,7 @@ namespace Nekoyume.UI
             var infos = state.GetArenaInfos(1, 3);
             if (States.Instance.CurrentAvatarState != null)
             {
-                // [TEEN Code Block Start]
+                // [TEN Code Block Start]
                 int upperListCount = 300;
                 int lowerListCount = 100;
 
@@ -485,7 +485,7 @@ namespace Nekoyume.UI
                     var address = state.OrderedArenaInfos.Last().AvatarAddress;
                     infos2 = state.GetArenaInfos(address, upperListCount, 0);
                 }
-                // [TEEN Code Block End]
+                // [TEN Code Block End]
 
                 infos.AddRange(infos2);
                 infos = infos.ToImmutableHashSet().OrderBy(tuple => tuple.rank).ToList();
